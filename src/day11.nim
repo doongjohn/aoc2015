eb4ccd4b0f5fe28a50282486dac4e983a95989e1
doongjohn <doongjohn@gmail.com>
2022-06-20 19:50:27 +0900

    update

--- src/day11.nim
+++ src/day11.nim
@@ -1,6 +1,6 @@
 import std/strutils
 
-proc incrementLowercaseString(password: var string) =
+proc incrementLowerCaseString(password: var string) =
   for i in countdown(password.high, 0):
     password[i] = (password[i].ord + 1).char
     if password[i] > 'z':
@@ -10,42 +10,42 @@ proc incrementLowercaseString(password: var string) =
 
 proc makeNextPassword(currentPassword: string): string =
   var password = currentPassword
-  block loop:
-    var ok = false
-    while not ok:
-      ok = true
 
-      # increment password
-      password.incrementLowercaseString()
+  var ok = false
+  while not ok:
+    ok = true
 
-      # i, o, or l is not ok
-      for c in password:
-        if c in "iol":
-          ok = false
-          break
+    # increment string
+    password.incrementLowerCaseString()
 
-      if ok:
-        # find abc, bcd, cde, and so on
-        var found = false
-        for i in 0 .. 5:
-          if password[i].ord + 1 == password[i + 1].ord and
-             password[i].ord + 2 == password[i + 2].ord:
-              found = true
-              break
-        ok = found
+    # i, o, or l is not ok
+    for c in password:
+      if c in "iol":
+        ok = false
+        break
 
-      if ok:
-        # find two different, non-overlapping pairs of letters
-        var c = '\0'
-        var i, n = 0
-        while i < 7:
-          defer: inc i
-          if password[i] != c and password[i] == password[i + 1]:
-            c = password[i]
-            inc i
-            inc n
-        if n < 2:
-          ok = false
+    if ok:
+      # find abc, bcd, cde, and so on
+      var found = false
+      for i in 0 .. 5:
+        if password[i].ord + 1 == password[i + 1].ord and
+           password[i].ord + 2 == password[i + 2].ord:
+            found = true
+            break
+      ok = found
+
+    if ok:
+      # find two different, non-overlapping pairs of letters
+      var c = '\0'
+      var i, n = 0
+      while i < 7:
+        defer: inc i
+        if password[i] != c and password[i] == password[i + 1]:
+          c = password[i]
+          inc i
+          inc n
+      if n < 2:
+        ok = false
 
   result = password
 
