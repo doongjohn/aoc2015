51c9064267c023a49475b49344fcb857d0f5c613
doongjohn <doongjohn@gmail.com>
2022-07-26 20:40:16 +0900

    day12 solved

--- src/day12.nim
+++ src/day12.nim
@@ -1,15 +1,20 @@
 import std/streams
+import std/strformat
+import std/parseutils
 
 proc puzzle1* =
   var inputs = openFileStream("input/day12.txt")
   defer: close inputs
 
   var sum = 0
+
+  # print answer: 111754
   defer: echo sum
 
   var num = 0
   var sign = 1
   var isPrevNum = false
+
   var c: char
   while (c = inputs.readChar(); c) != '\0':
     if c in '0' .. '9':
@@ -27,7 +32,54 @@ proc puzzle1* =
         sign = 1
       isPrevNum = false
 
+proc findInObject(input, find: string): int =
+  result = -1
+  let findstr = ":\"{find}\"".fmt
+  let length = findstr.high
+
+  var i = 0
+  var level = 0
+  var found = false
+  while i < input.len:
+    defer: inc i
+
+    if input[i] in "{":
+      inc level
+    elif input[i] in "}":
+      dec level
+      if level == 0:
+        if found:
+          result = i
+        break
+
+    if level > 1 or found:
+      continue
+
+    if i + length < input.len and input[i .. i + length] == findstr:
+      found = true
+
 proc puzzle2* =
-  # TODO: later
-  var inputs = openFileStream("input/day12.txt")
-  defer: close inputs
+  let input = readFile("input/day12.txt")
+  # let input = """[1,{"a":{"a":1},"b":"red",{1}},1],{1,"a":"red"}"""
+
+  var sum = 0
+
+  # print answer: 65402
+  defer: echo sum
+
+  var i = 0
+  while i < input.len:
+    defer: inc i
+
+    if input[i] in "{":
+      let redObjEnd = input[i .. ^1].findInObject("red")
+      if redObjEnd > 0:
+        i += redObjEnd
+        continue
+
+    if input[i] == '-' or input[i] in '0' .. '9':
+      var num = 0
+      let numParsed = input.parseInt(num, i)
+      if numParsed > 0:
+        sum += num
+        i += numParsed
--- src/main.nim
+++ src/main.nim
@@ -1,40 +1,7 @@
-# import day1
-# import day2
-# import day3
-# import day4
-# import day5
-# import day6
-# import day7
-# import day8
-# import day9
-# import day10
-# import day11
 import day12
 
 proc main =
-  # day1.puzzle1()
-  # day1.puzzle2()
-  # day2.puzzle1()
-  # day2.puzzle2()
-  # day3.puzzle1()
-  # day3.puzzle2()
-  # day4.puzzle1()
-  # day4.puzzle2()
-  # day5.puzzle1()
-  # day5.puzzle2()
-  # day6.puzzle1()
-  # day6.puzzle2()
-  # day7.puzzle1()
-  # day7.puzzle2()
-  # day8.puzzle1()
-  # day8.puzzle2()
-  # day9.puzzle1()
-  # day9.puzzle2()
-  # day10.puzzle1()
-  # day10.puzzle2()
-  # day11.puzzle1()
-  # day11.puzzle2()
-  day12.puzzle1()
-  # day12.puzzle2()
+  # day12.puzzle1()
+  day12.puzzle2()
 
 main()
